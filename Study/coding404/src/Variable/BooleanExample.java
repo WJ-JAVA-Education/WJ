15cfa8899da139bbae7c53ac8d5cb126cb2821c7
DESKTOP-PC86ECB\curong <curong@outlook.kr>
2020-12-19 20:02:53 +0900

    강의 듣는중

---
+++ Study/coding404/src/Variable/BooleanExample.java
@@ -0,0 +1,15 @@
+package Variable;
+
+public class BooleanExample {
+	public static void main(String[] args) {
+		boolean b1 = true;
+		boolean b2 = false;
+		//boolean b3 = 0;
+		//boolean b4 = 0;
+		
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		
+	}
+}
