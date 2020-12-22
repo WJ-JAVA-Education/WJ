de873514c526b655486b47f02471072835cc2b36
DESKTOP-I046TNV\WooJ <curong@outlook.kr>
2020-12-23 00:35:31 +0900

    study~
    1

---
+++ Study/CodeUp/src/basic/Quiz_14.java
@@ -0,0 +1,20 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Quiz_14 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		
+		char a = sc.next().charAt(0);
+		char b = sc.next().charAt(0);
+
+
+		System.out.printf("%c %c", a, b);
+
+	}
+
+}
