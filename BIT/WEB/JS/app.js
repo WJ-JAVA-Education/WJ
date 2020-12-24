85a1b420c5b66d32104c57ffb39dab9dd8d9bb06
DESKTOP-PC86ECB\curong <curong@outlook.kr>
2020-12-24 14:43:01 +0900

    백그라운드 생성기 및 주사위
    1

--- BIT/WEB/JS/app.js
+++ BIT/WEB/JS/app.js
@@ -1,8 +1,2 @@
-<script>
-var id = prompt ("아이디를 입력해 주세요.");
-if(id==='BTI'){
-    var pass = prompt ("비밀번호를 입력해 주세요");
-    if(pass==='504'){
-        alert("일치합니다");}
-    }else { alert("일치 하지않습니다")};
-</script>
+alert('hi');
+console.log('콘솔출력');
