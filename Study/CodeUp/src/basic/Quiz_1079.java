c5a2a7124fb0f04b0f0474104b59f74cd533dfc3
DESKTOP-PC86ECB\curong <curong@outlook.kr>
2021-01-03 01:27:37 +0900

    1
    1

--- Study/CodeUp/src/basic/Quiz_1079.java
+++ Study/CodeUp/src/basic/Quiz_1079.java
@@ -6,12 +6,23 @@ public class Quiz_1079 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		
-		String[] a = sc.nextLine().split(" ");
 		
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
+		while (true) {
+			char a = sc.next().charAt(0);
+
+			System.out.println(a);
+			if (a=='q') break;
 		}
+
+
+
+
+
+		
+		
+		
 		
 		
 		
