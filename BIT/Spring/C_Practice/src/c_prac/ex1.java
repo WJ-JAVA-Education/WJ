f77a4d2638ff08e04b7dfe725dc150a3232bcbe9
curong <73506250+curong@users.noreply.github.com>
2021-02-10 16:55:41 +0900

    크롤링 커밋

--- BIT/Spring/C_Practice/src/c_prac/ex1.java
+++ BIT/Spring/C_Practice/src/c_prac/ex1.java
@@ -1,27 +1,50 @@
 package c_prac;
 
+import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
 public class ex1 {
-	login log = new login();
 
+	/*
+     *	Document 클래스 : 연결해서 얻어온 HTML 전체 문서
+     *	Element 클래스  : Documnet의 HTML 요소
+     *	Elements 클래스 : Element가 모인 자료형
+	 */
+	
 	public static void main(String[] args) {
-		String url = "http://www.thevapor.co.kr/";
+		// Jsoup를 이용해서 네이버 스포츠 크롤링
+		String url = "https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com";
+		Document doc = null;
+		
 		try {
-			Document doc =	Jsoup.connect(url).get();
-			Elements html = doc.select("a");
-			for (Element element : html) {
-				
-				
-			}
-		}catch (Exception e) {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
+		// 주요 뉴스로 나오는 태그를 찾아서 가져오도록 한다.
+		Elements element = doc.select("div.qr_once_inner");
 		
-	}		
+		// 1. 헤더 부분의 제목을 가져온다.
+		String title = element.select("p").text();
+
+		System.out.println("============================================================");
+		System.out.println(title);
+		System.out.println("============================================================");
+		
+		
+		
+		for(Element el : element) {	// 하위 뉴스 기사들을 for문 돌면서 출력
+			Elements el1 = element.select("p");
+			System.out.println(el1.text());
+		}
+		
+		System.out.println("============================================================");
+	}
+
 }
+
+
