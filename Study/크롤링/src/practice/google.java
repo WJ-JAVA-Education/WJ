1e278da1489649d7871be4ff28ee6e8b9a796c6b
DESKTOP-PC86ECB\curong <curong@outlook.kr>
2021-02-13 21:34:30 +0900

    크롤링 css , google 뉴스 가져오기 성공 (로그인 질문)

--- Study/크롤링/src/practice/google.java
+++ Study/크롤링/src/practice/google.java
@@ -5,59 +5,84 @@ import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
 
 public class google {
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Document doc = null;
 
-	public static void main(String[] args) throws IOException {
-		GoogleNews("aa");
+		GoogleNewsSreach(doc, "연예인");
+		return;
 	}
+	public static void GoogleNewsSreach(Document doc, String Search) throws IOException, InterruptedException {
+		Thread.sleep(2000);
+		String GOOGLE ="https://www.google.com"; // 앞에 구글을 붙혀주기 위함
+		if(doc == null) {
+			String URL ="https://www.google.com/search?q="+Search+"&tbm=nws&start=0";
+			doc = Jsoup.connect(URL).get();
+		}
+		
+		if(doc.select("td.d6cvqb").select("a").select("span").text().contains("다음")) {
+			List<String> PageSeletor = doc.select("td.d6cvqb").select("a").eachAttr("href");
+			if(PageSeletor.size() == 1) {
+				PageSeletor.add(0, "");
+			}
+				  List<String> el = doc.getElementsByClass("JheGif nDgy9d").eachText(); 
+				  List<String> el2 = doc.getElementsByClass("dbsr").select("a").eachAttr("href");
+				  for (int j = 0; j < el.size(); j++) {
+					  System.out.println();
+					  System.out.println(el.get(j)+"\n"+el2.get(j));
+					  
+				  }
+				  Document doc1 = Jsoup.connect(GOOGLE + PageSeletor.get(1)).get();
+				  
+			GoogleNewsSreach(doc1, GOOGLE + PageSeletor.get(1));
+		} else {
+			
+			return;
+		}
+	}
+	
+//	public static void P_GoogleNews(String Search)throws IOException {
+//		String URL ="https://www.google.com/search?q="+Search+"&tbm=nws";
+//		String GOOGLE ="https://www.google.com"; // 앞에 구글을 붙혀주기 위함
+//		
+//		//검색 된 페이지의 페이지를 가져오는 과정
+//		Document doc = Jsoup.connect(URL).get();
+//		String nextPage = doc.select("td.d6cvqb").select("a").attr("href");
+//		
+//		while(doc.select("td.d6cvqb").hasAttr("role")) {
+//			  Document doc1 = Jsoup.connect(GOOGLE + nextPage).get();
+//			  List<String> el = doc.getElementsByClass("JheGif nDgy9d").eachText(); 
+//			  List<String> el2 = doc.getElementsByClass("dbsr").select("a").eachAttr("href"); 
+//			  for (int j = 0; j < el.size(); j++) {
+//				  System.out.println(); 
+//				  System.out.println(el.get(j)+"\n"+el2.get(j));
+//			  }
+//			  GoogleNextPageBtn("aa");
+//		}
+//	}
 	
 	
 	/*
-	public static void P_GoogleNews(String Search)throws IOException {
-		String URL ="https://www.google.com/search?q="+Search+"&tbm=nws";
-		String GOOGLE ="https://www.google.com";
-		
-		//검색 된 페이지의 페이지를 가져오는 과정
-		Document SearchPage = Jsoup.connect(URL).get();
-		List<String> pagelink = SearchPage.getElementsByClass("AaVjTc").select("a").eachAttr("href");
-		
-		// pagelink 를 배열에 담는 과정
-		String[] pagearr = new String[pagelink.size()];
-		for (int i = 0; i < pagelink.size(); i++) {
-			pagearr[i] = GOOGLE+pagelink.get(i);
-		}
-		
-		// 구글 해당 검색어의 뉴스를 가져오는 과정
-		
-		  for (int i = 0; i < pagearr.length; i++) {
-		  Document doc = Jsoup.connect(pagearr[i]).get(); 
-		  List<String> el = doc.getElementsByClass("JheGif nDgy9d").eachText(); 
-		  List<String> el2 = doc.getElementsByClass("dbsr").select("a").eachAttr("href"); 
-		  for (int j = 0; j < el.size(); j++) {
-			  System.out.println();
-			  System.out.println(el.get(j)+"\n"+el2.get(j));
-		  }
-		}
-	}
-	*/
-	
-	
-	
 	public static void GoogleNews(String Search)throws IOException{
 		String URL ="https://www.google.com/search?q="+Search+"&tbm=nws";
 		String GOOGLE ="https://www.google.com";
 		
 		//검색 된 페이지의 페이지를 가져오는 과정
 		Document SearchPage = Jsoup.connect(URL).get();
-		if (pagelink == null) {
-		for (int i = 0; i < array.length; i++) {
-			List<String> pagelink = SearchPage.getElementsByClass("d6cvqb").select("a").eachAttr("href");
-			String[] pagearr = new String[pagelink.size()];
-		}
 		
+
+			for (int i = 0; i < ifpage.size(); i++) {
+				List<String> pagelink = SearchPage.getElementsByClass("d6cvqb").select("a").eachAttr("href");
+				String[] pagearr = new String[pagelink.size()];
+				pagearr[i] = GOOGLE+pagelink.get(i);
+				System.out.println(pagearr[i]);
+					Document page = Jsoup.connect(pagearr[i]).get();
+				}
+			}
+		*/
 		
 		
 		
@@ -65,6 +90,8 @@ public class google {
 	
 	
 	
-}
+
+
+
 }
 
--- Study/크롤링/src/practice/practice1.java
+++ Study/크롤링/src/practice/practice1.java
@@ -1,47 +1,57 @@
 package practice;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.OutputStreamWriter;
 import java.util.List;
-import java.util.StringTokenizer;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 public class practice1 {
+	
 
-	public static void main(String[] args) throws IOException {
-		String url = "http://www.thevapor.co.kr/category/%EB%94%94%EC%A0%80%ED%8A%B8%EC%9D%8C%EC%8B%9D%ED%96%A5/37/";
+//	public static void main(String[] args) throws IOException {
+//		String url = "http://www.thevapor.co.kr/category/%EB%94%94%EC%A0%80%ED%8A%B8%EC%9D%8C%EC%8B%9D%ED%96%A5/37/";
+//		
+//			Document doc = Jsoup.connect(url).get();
+//			
+//			
+//			String el = doc.getElementsByClass("item xans-record-").select("a").text().replaceAll("상품명 : ","\n");
+//			
+//			String a = "http://www.thevapor.co.kr";
+//			List<String> el2 = doc.getElementsByClass("item xans-record-").select("a").eachAttr("href");
+//				StringBuilder sb = new StringBuilder();
+//				for(int i = 0; i < el2.size(); i++) {
+//					sb.append(a + el2.get(i));
+//					sb.append("\n");
+//				}
+//			System.out.println(sb);
+//			StringTokenizer tk = new StringTokenizer(sb.toString());
+//			List<String> listTk = new ArrayList<String>();
+//			try {
+//				while(tk.hasMoreTokens()) {
+//					listTk.add((String) tk.nextElement());
+//				}
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//
+//			for (int i = 0; i < listTk.size(); i++) {
+//				Document doc2 = Jsoup.connect(listTk.get(i)).get();
+//			}
 		
-			Document doc = Jsoup.connect(url).get();
-			
-			
-			String el = doc.getElementsByClass("item xans-record-").select("a").text().replaceAll("상품명 : ","\n");
-			
-			String a = "http://www.thevapor.co.kr";
-			List<String> el2 = doc.getElementsByClass("item xans-record-").select("a").eachAttr("href");
-				StringBuilder sb = new StringBuilder();
-				for(int i = 0; i < el2.size(); i++) {
-					sb.append(a + el2.get(i));
-					sb.append("\n");
-				}
-			System.out.println(sb);
-			StringTokenizer tk = new StringTokenizer(sb.toString());
-			List<String> listTk = new ArrayList<String>();
-			try {
-				while(tk.hasMoreTokens()) {
-					listTk.add((String) tk.nextElement());
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 
-			for (int i = 0; i < listTk.size(); i++) {
-				Document doc2 = Jsoup.connect(listTk.get(i)).get();
-				System.out.println(doc2);
-			}
-
+				   public static void main(String[] args) throws IOException {
+				      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+				      crawling theVapor = new crawling();
+				      List<String[]> products = theVapor.theVaporProducts();
+				      for(int i = 0; i < products.size(); i++) {
+				         String[] strArr = products.get(i);
+				         for(int j = 0; j < strArr.length; j++) {
+				            bw.write(strArr[i]);
+				         }
+				         bw.write("\n");
+				      }
+				      bw.flush();
+				      bw.close();
+				   }
 			
-			
-	}
 	}
