2edf98f319061e1210255badbc3818898300438c
DESKTOP-I046TNV\WooJ <curong@outlook.kr>
2021-04-14 15:17:35 +0900

    회원조회 및 생성 기능

--- Study/JAVA/Web/Spring/infrun_Kim/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Study/JAVA/Web/Spring/infrun_Kim/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,8 +3,11 @@
  */
 package hello.hellospring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -36,6 +39,13 @@ public class MemberController {
 	memberService.join(member);
 	
 	return "redirect:/";
+    }
+    
+    @GetMapping("/members")
+    public String list(Model model) {
+	List<Member> members = memberService.findMembers();
+	model.addAttribute("members", members);
+	return "members/memberList";
     }
     
     
