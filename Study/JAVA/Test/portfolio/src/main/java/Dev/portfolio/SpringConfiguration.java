d069d98dea9030a681b14ea75cc1d109091fc283
curong <73506250+curong@users.noreply.github.com>
2021-04-28 17:05:44 +0900

    [Spring Boot] - 타임리프 CSS 적용

---
+++ Study/JAVA/Test/portfolio/src/main/java/Dev/portfolio/SpringConfiguration.java
@@ -0,0 +1,15 @@
+package Dev.portfolio;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+//@org.springframework.context.annotation.Configuration
+@SpringBootConfiguration
+public class SpringConfiguration {
+
+//	@Bean
+//	public DataSource datasource() {
+//		return DataSourceBuilder.create().driverClassName("").url("").username("").password("").build();
+//	}
+	
+	
+}
