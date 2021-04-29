6eba1e505ba334fc56c6619d1b486e0cb505cb9c
curong <curong@outlook.kr>
2021-04-29 21:38:17 +0900

    [portfolio] - 사용기술 a 태그 넣어서 링크 이동 완료.

--- Study/JAVA/Test/portfolio/src/main/java/Dev/portfolio/Controller/MappingController.java
+++ Study/JAVA/Test/portfolio/src/main/java/Dev/portfolio/Controller/MappingController.java
@@ -10,5 +10,43 @@ public class MappingController {
 	public String index() {
 		return "index";
 	}
+	
+	@GetMapping("/HTML5")
+	public String HTML5() {
+		return "/skill/HTML5";
+	}
+	
+	@GetMapping("/CSS3")
+	public String CSS3() {
+		return "/skill/CSS3";
+	}
+	@GetMapping("/Java")
+	public String Java() {
+		return "/skill/Java";
+	}
+	@GetMapping("/JavaScript")
+	public String JavaScript() {
+		return "/skill/JavaScript";
+	}
+	@GetMapping("/Spring")
+	public String Spring() {
+		return "/skill/Spring";
+	}
+	
+	@GetMapping("/SpringBoot")
+	public String SpringBoot() {
+		return "/skill/SpringBoot";
+	}
+	
+	@GetMapping("/DataBase")
+	public String DataBase() {
+		return "/skill/DataBase";
+	}
+	
+	@GetMapping("/Thymeleaf")
+	public String Thymeleaf() {
+		return "/skill/Thymeleaf";
+	}
+	
 
 }
