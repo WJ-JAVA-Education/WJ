8cb07f7ae679e13b06da8cbcf67554b32c34c0fc
curong <curong@outlook.kr>
2021-05-07 00:01:59 +0900

    [CodingTest] 카카오 1레벨

---
+++ Study/JAVA/Test/CoTest/src/Exam.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Exam {
+    public static void main(String[] args) {
+        int count;
+        int[] arr = {1,2,3};
+        for (int i = 0; i ;)
+    }
+}
