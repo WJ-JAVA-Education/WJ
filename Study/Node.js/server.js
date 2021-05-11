9160ab74d4e8e9a74237a6a495ffde4b58abc053
curong <curong@outlook.kr>
2021-05-11 17:18:31 +0900

    [NodeJS] MongoDB 연동중

--- Study/Node.js/server.js
+++ Study/Node.js/server.js
@@ -1,5 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dbUrl = 'mongodb://@127.0.0.1:27017/NodeJS';
 const app = express();
+
+const connect = () => {
+  if (process.env.NODE_ENV !== 'production') {
+      mongoose.set('debug', true);
+  }
+  mongoose.connect(dbUrl, {
+      dbName: 'NodeJS',
+      useNewUrlParser: true,
+      useCreateIndex: true,
+  }, (err) => {
+      if (err) {
+          console.error('몽고디비 연결 에러', err);
+      } else {
+          console.log('몽고디비 연결 성공');
+      }
+  });
+};
+mongoose.connection.on('error', (err) => {
+  console.error('몽고디비 연결 에러', err);
+});
+mongoose.connection.on('disconnected', () => {
+  console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.');
+  connect();
+});
+
+module.exports = connect;
+
+
 
 app.use(express.static(__dirname + '/css'));
 
