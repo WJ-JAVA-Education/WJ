8d1c58b3adf0af65c5afc3a5f1632a13fce870f4
curong <curong@outlook.kr>
2021-05-12 16:54:19 +0900

    [NodeJS] - 몽고디비 클러스트 추가 ,

--- Study/Node.js/server.js
+++ Study/Node.js/server.js
@@ -1,34 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dbUrl = 'mongodb://@127.0.0.1:27017/NodeJS';
+const dbUrl = 'mongodb+srv://root:1234@wooj.fynod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const app = express();
 
-const connect = () => {
-  if (process.env.NODE_ENV !== 'production') {
-      mongoose.set('debug', true);
-  }
-  mongoose.connect(dbUrl, {
-      dbName: 'NodeJS',
-      useNewUrlParser: true,
-      useCreateIndex: true,
-  }, (err) => {
-      if (err) {
-          console.error('몽고디비 연결 에러', err);
-      } else {
-          console.log('몽고디비 연결 성공');
-      }
-  });
-};
-mongoose.connection.on('error', (err) => {
-  console.error('몽고디비 연결 에러', err);
-});
-mongoose.connection.on('disconnected', () => {
-  console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.');
-  connect();
-});
-
-module.exports = connect;
-
+mongoose.connect('mongodb+srv://root:1234@wooj.fynod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' , {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true , 
+    useCreateIndex: true , 
+    useFindAndModify: false
+}).then(() => console.log ("MongoDB Connected...")).catch(err => console.log(err))
 
 
 app.use(express.static(__dirname + '/css'));
