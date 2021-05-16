03dd7ca8914112aa4bc553077f3c13a3cac743a9
curong <curong@outlook.kr>
2021-05-17 00:17:30 +0900

    [NodeJS] - 스키마 추가 , 몽고디비 깃허브 숨기기 구현중

---
+++ Study/Node.js/config/dev.js
@@ -0,0 +1,3 @@
+module.exports ={
+  mongoURI:'mongodb+srv://root:1234@wooj.fynod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- Study/Node.js/server.js
+++ Study/Node.js/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dbUrl = 'mongodb+srv://root:1234@wooj.fynod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const app = express();
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 mongoose.connect(dbUrl,{
   useNewUrlParser: true, 
@@ -17,6 +22,15 @@ app.listen(80 , function(){
   console.log('listening on 80')
 });
 
+app.post('/register', (req,res) => {
+  const user = new User(req.body)
+    user.save((err, userInfo) => {
+      if (err) return res.json({ success: false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
+});
 
 app.get('/pet' , function(req,res){
   res.send('펫용품을 쇼핑 할 수 있는 사이트 입니다.');
