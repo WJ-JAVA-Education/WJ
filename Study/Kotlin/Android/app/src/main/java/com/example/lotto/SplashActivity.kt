71c7348b57789716269a78afb7e722c8808119c7
curong <curong@outlook.kr>
2021-05-27 23:59:47 +0900

    [Kotlin-Android] - xml 에서 findViewbyId 안하고 바로 찾아오기
    gradle로 간 후 id에
    id 'kotlin-android-extensions'
    추가

--- Study/Kotlin/Android/app/src/main/java/com/example/lotto/SplashActivity.kt
+++ Study/Kotlin/Android/app/src/main/java/com/example/lotto/SplashActivity.kt
@@ -2,10 +2,15 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        animationView.setOnClickListener {
+
+        }
     }
 }
