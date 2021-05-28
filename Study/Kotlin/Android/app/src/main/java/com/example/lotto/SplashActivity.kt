202c89e8714739884d31c2674da98b39cef602e7
curong <curong@outlook.kr>
2021-05-28 16:55:45 +0900

    [Kotlin-Android] - 로또번호 생성기 구현

--- Study/Kotlin/Android/app/src/main/java/com/example/lotto/SplashActivity.kt
+++ Study/Kotlin/Android/app/src/main/java/com/example/lotto/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.example.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
@@ -9,8 +12,36 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        /* 1번
         animationView.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        */
 
+        val handler = Handler(Looper.getMainLooper()) // 3번
+        val runable = Runnable {  // 4번
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        handler.postDelayed(runable, 3000) // 5번
+
+        animationView.setOnClickListener {  //6번
+            handler.removeCallbacks(runable)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+
+            /* 2번
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+         */
         }
     }
 }
