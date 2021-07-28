090859cf1f9b6f94a77df77baf17964f15623376
curong <curong@outlook.kr>
2021-07-28 20:10:20 +0900

    크롤링 확인

--- Study/JAVA/크롤링/src/practice/naver.java
+++ Study/JAVA/크롤링/src/practice/naver.java
@@ -8,19 +8,20 @@ import org.jsoup.nodes.Document;
 
 public class naver {
 
-	public static void main(String[] args) throws IOException {
-		String search = "전자담배";
-			long startTime = System.currentTimeMillis();
-			for (int j = 0; j < 50; j+=10) {
-				String URL = "https://search.naver.com/search.naver?&where=news&query="+search+"&sm=tab_pge&sort=1&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so:dd,p:all,a:all&mynews=0&start="+j+"&refresh_start=0";
-				Document doc = Jsoup.connect(URL).get();
-				List<String> title = doc.getElementsByClass("news_tit").eachText();
-				List<String> href = doc.getElementsByClass("news_tit").eachAttr("href");
-			for (int i = 0; i < title.size(); i++) {
-				System.out.println(title.get(i)+"\n"+href.get(i)+"\n");
-			}
-			
-	}
-			long endTime = System.currentTimeMillis();
-			System.out.println(endTime-startTime);
-}}
+    public static void main(String[] args) throws IOException {
+        String search = "올림픽";
+        long startTime = System.currentTimeMillis();
+        for (int j = 0; j < 50; j += 10) {
+            String URL = "https://search.naver.com/search.naver?&where=news&query=" + search + "&sm=tab_pge&sort=1&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so:dd,p:all,a:all&mynews=0&start=" + j + "&refresh_start=0";
+            Document doc = Jsoup.connect(URL).get();
+            List<String> title = doc.getElementsByClass("news_tit").eachText();
+            List<String> href = doc.getElementsByClass("news_tit").eachAttr("href");
+            for (int i = 0; i < title.size(); i++) {
+                System.out.println(title.get(i) + "\n" + href.get(i) + "\n");
+            }
+
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
+}
